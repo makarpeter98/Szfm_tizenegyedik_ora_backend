3940cc01387af66152aceb78a9289361fa9a1371
makar <makar.peter98@gmail.com>
2024-11-25 14:11:40 +0100

    initial commit

---
+++ src/main/java/hu/szfm/makar/React_demo/ReactDemoApplication.java
@@ -0,0 +1,13 @@
+package hu.szfm.makar.React_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/szfm/makar/React_demo/ReactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.szfm.makar.React_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
