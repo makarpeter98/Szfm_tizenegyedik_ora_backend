2090a6e47a08a62c0795b591d289d7d1472a8d77
makar <makar.peter98@gmail.com>
2024-11-26 08:25:02 +0100

    Person osztály kész

--- src/main/java/hu/szfm/makar/Application/modell/Person.java
+++ src/main/java/hu/szfm/makar/Application/modell/Person.java
@@ -1,4 +1,25 @@
 package hu.szfm.makar.Application.modell;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Person {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String username;
+    private String email;
+
 }
