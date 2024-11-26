f9299afc1f388214be7ec1cb93b347428d21639d
makar <makar.peter98@gmail.com>
2024-11-26 08:45:58 +0100

    application.properties kijavítva

--- src/main/java/hu/szfm/makar/Application/controller/PersonController.java
+++ src/main/java/hu/szfm/makar/Application/controller/PersonController.java
@@ -1,4 +1,15 @@
 package hu.szfm.makar.Application.controller;
 
+import hu.szfm.makar.Application.modell.Person;
+import hu.szfm.makar.Application.modell.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@CrossOrigin("http.//localhost:3000")
 public class PersonController {
+    //@Autowired
+   // private PersonRepository personRepository;
+
 }
