61c0c2fd824705253b6375dcce6d8524f916998f
makar <makar.peter98@gmail.com>
2024-11-26 08:53:18 +0100

    PersonController kész

--- src/main/java/hu/szfm/makar/Application/controller/PersonController.java
+++ src/main/java/hu/szfm/makar/Application/controller/PersonController.java
@@ -3,13 +3,26 @@ package hu.szfm.makar.Application.controller;
 import hu.szfm.makar.Application.modell.Person;
 import hu.szfm.makar.Application.modell.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//@RestController
-//@CrossOrigin("http.//localhost:3000")
+import java.util.List;
+
+@RestController
+@CrossOrigin("http.//localhost:3000")
 public class PersonController {
-    //@Autowired
-   // private PersonRepository personRepository;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @PostMapping("/person")
+    Person newPerson(@RequestBody Person newPerson)
+    {
+        return personRepository.save(newPerson)
+    }
+
+    @GetMapping("/persons")
+    List<Person> getAllPersons()
+    {
+        return personRepository.findAll();
+    }
 
 }
